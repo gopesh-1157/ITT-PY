4abae230a2767f8014f38595e161b6d9087c3dac
GOPESH RATHINAM <gopeshrathnam_bcs28@mepcoeng.ac.in>
2026-04-20 13:06:48 +0700

    Add Sum and Average calculation for two numbers

---
+++ SumAndAverageofTwoNumber.py
@@ -0,0 +1,9 @@
+n1=int(input("enter the number 1 :"))
+n2=int(input("enter the number 2 :"))
+if (0<=n1<=100 and 0<=n2<=100):
+   a=n1+n2;
+   b=a/2;
+   print("sum :" ,a)
+   print("average :" ,b)
+else:
+   print("invalid")
