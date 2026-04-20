582e74c061afe38525758d3753c85cf71956e07c
GOPESH RATHINAM <gopeshrathnam_bcs28@mepcoeng.ac.in>
2026-04-20 13:34:49 +0700

    Create SlabCheck.py

---
+++ SlabCheck.py
@@ -0,0 +1,2 @@
+salary=int(input("enter salary:"))
+print("no tax") if salary<=250000 else print("tax amount:",salary*0.005) if 250000<salary<500000 else print("tax amount:",salary*0.1) if 500000<salary<1000000 else print("tax amount:",salary*0.15)
